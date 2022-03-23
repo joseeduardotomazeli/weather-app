f7655bc0141454cb7275ada7e33a079e7939bc42
joseeduardotomazeli <joseeduardo.tomazeli@outlook.com>
2022-03-23 19:58:55 -0300

    refactor: edit routes folder location

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import Routes from './pages/routes';
+import Routes from './routes';
 
 function App() {
   return <Routes />;
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,6 +1,6 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
-import Home from '../Home';
+import Home from '../pages/Home';
 
 function AppRoutes() {
   return (
